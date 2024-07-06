dc0a2aeecc718cb50a01e8d6924bf5720db9c41d
Yara Elkady <51079151+Yara484@users.noreply.github.com>
2024-07-06 16:49:16 +0300

    Time: 61 ms (8.38%) | Memory: 17.1 MB (44.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,10 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        res = []
-        for c in range(len(s) - 1, -1, -1):
-            if s[c].isalnum():
-                res.append(s[c])
-        
-        s = ''.join([char for char in s if char.isalnum()])
-        res = "".join(res)    
-        return res.lower() == s.lower()  
+        word1 = ''
+        word2 = ''
+        s = s.lower()
+
+        for char in s:
+            if char.isalnum():
+                word1 += char
+
+
+        for i in range(len(s)-1, -1, -1):
+            if s[i].isalnum():
+                word2 += s[i]
+
+        return word1 == word2                
