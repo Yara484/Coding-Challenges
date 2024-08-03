cd22e0e7a13cbd88b5ab046b1af32fee7bba3597
Yara Elkady <51079151+Yara484@users.noreply.github.com>
2024-08-03 14:55:51 +0300

    Time: 85 ms (77.09%) | Memory: 22.1 MB (11.47%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        farray = [[] for i in range(len(nums) + 1)]
-
+        # Frequency Array
+        # Create array with length of nums
+        farray = [[] for i in range(len(nums)+1)]
+        mymap = {}
         res = []
+        # Hashmap of values and their frequencies
         for i in nums:
-            count[i] = count.get(i, 0) + 1  
-
-        for num, freq in count.items():
-            farray[freq].append(num)   
-
+            mymap[i] = mymap.get(i, 0) + 1
+        # Populating Freq array
+        for key, value in mymap.items():
+            farray[value].append(key)  
+        # Building result string by iterating in reverse
         for i in range(len(farray)-1, 0, -1):
-            for n in farray[i]:
-                res.append(n)
+            for j in farray[i]:
+                res.append(j) 
                 if len(res) == k:
                     return res
