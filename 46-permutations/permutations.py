c0cfcb9f05ae7c649cb4973854de9e5af9f23ddc
Yara Elkady <51079151+Yara484@users.noreply.github.com>
2024-08-19 22:46:26 +0300

    Time: 39 ms (67.24%) | Memory: 16.5 MB (90.04%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res, sol = [], []
+        def backtrack():
+            # Base Case
+            if len(sol) == len(nums):
+                res.append(sol[:])
+                return
+            # Decision To Choose
+            for i in nums:
+                if i not in sol:
+                    sol.append(i)
+                    backtrack()
+                    sol.pop()
+
+        backtrack()
+        return res                
