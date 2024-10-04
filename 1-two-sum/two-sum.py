670a4b0f50909c3859df0b807bdf66d0069c422d
Yara Elkady <51079151+Yara484@users.noreply.github.com>
2024-10-04 16:52:20 +0300

    Time: 55 ms (80.36%) | Memory: 17.8 MB (35.09%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Using Hashmap
-        mymap = {}
+        myMap = {}
 
         for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in mymap:
-                return [mymap[diff], i]
-            mymap[nums[i]] = i
-                
+            val = target - nums[i]
+            if val in myMap:
+                return [myMap[val], i]
+            else:
+                myMap[nums[i]] = i
+""""
+nums = [2,7,11,15], target = 9
+i = 0
+val = 9 - 2 = 7, 
+{
+    2 : 0
+}
+
+
+"""
