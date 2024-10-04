a6aadb6fb86591c1d67897e9f30498edfddd5b4f
Yara Elkady <51079151+Yara484@users.noreply.github.com>
2024-10-05 00:37:03 +0300

    Time: 48 ms (70.72%) | Memory: 16.6 MB (58.76%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.py
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -1,20 +1,16 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        directions = {
-            'U' : [0,1],
-            'D' : [0,-1],
-            'R' : [1,0],
-            'L' : [-1,0]
-        }
-        origin = [0,0]
-        for char in moves:
-            move = directions[char]
-            origin[0] += move[0]
-            origin[1] += move[1]
+        x, y = 0, 0
+        for move in moves:
+            if move == 'U':
+                y += 1
+            elif move == 'D':
+                y -= 1  
+            elif move == 'R':
+                x += 1  
+            elif move == 'L':
+                x -= 1
 
-        if origin[0] == 0 and origin[1] == 0:
-            return True
-        return False
-
+        return x == 0 and y == 0                    
         
         
