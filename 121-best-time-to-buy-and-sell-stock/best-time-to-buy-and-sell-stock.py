6dd22b0bf12b500021e0fa584d1c4639e163f385
Yara Elkady <51079151+Yara484@users.noreply.github.com>
2024-10-05 00:19:46 +0300

    Time: 783 ms (23.05%) | Memory: 27.5 MB (12.87%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # Using two pointers, l is buy and r is sell
-        res = 0
+        # Sliding Window
+        # buy has to be smaller than sell
         l, r = 0, 1
-
-        while r < len(prices):
-            profit = prices[r] - prices[l]
-            res = max(res, profit)
-            if prices[r] < prices[l]:
+        profit = 0
+        for i in range(len(prices)-1):
+            profit = max(profit, prices[r] - prices[l])
+            if prices[l] > prices[r]:
                 l = r
             r += 1
-        return res            
+
+        return profit
+
+"""
+ 0,1,2,3,4,5
+[7,1,5,3,6,4]
+l = 0, 1
+r = 1, 2, 3, 4
+profit = 0, 4, 5
+"""
