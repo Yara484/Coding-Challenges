42dfb0f5972d86c7bfca4ae25b5d10a49ec0c215
Yara Elkady <51079151+Yara484@users.noreply.github.com>
2024-10-07 02:29:55 +0300

    Time: 75 ms (98.02%) | Memory: 20.2 MB (53.34%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,19 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Using a  hashmap to store char frequency
-        # key is sorted string
-        mymap = {}
-        for word in strs:
-            Sword = sorted(word) # ['a', 'c', 't']
-            Sword = ''.join(Sword)      # 'act'
-            if Sword in mymap:
-                mymap[Sword].append(word)
-            else:
-                mymap[Sword] = [word]  
+        myMap = {}
 
-        # Now I have to build the result array
-        res = []
-        for i in mymap:
-            res.append(mymap[i])
-        return res              
-    
+        for word in strs:
+            Sword = sorted(word)
+            Sword = "".join(Sword)
+            if Sword in myMap:
+                myMap[Sword].append(word)
+            else:
+                myMap[Sword] = [word]
+
+        return myMap.values()            
+            
