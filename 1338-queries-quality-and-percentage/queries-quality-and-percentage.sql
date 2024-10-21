f474a6c68a6ee10a2564be05afe965d368c10d37
Yara Elkady <51079151+Yara484@users.noreply.github.com>
2024-10-21 22:03:05 +0300

    Time: 504 ms (10.27%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,29 +1,5 @@
-WITH cte1 AS (
-    SELECT query_name, COUNT(*) AS alls
-    FROM Queries
-    GROUP BY query_name
-),
-
-cte2 AS (
-    SELECT query_name, COUNT(query_name) AS part
-    FROM Queries
-    WHERE rating < 3
-    GROUP BY query_name
-),
-
-cte3 AS (
-    SELECT t1.query_name, ROUND((part / alls) * 100, 2) AS poor_query_percentage
-    FROM cte1 t1 
-    left JOIN cte2 t2 ON t1.query_name = t2.query_name
-),
-
-cte4 AS (
-    SELECT query_name, ROUND(AVG(rating / position), 2) AS quality
-    FROM Queries
-    GROUP BY query_name
-)
-
-SELECT cte4.query_name, quality, COALESCE(poor_query_percentage,0) AS poor_query_percentage 
-FROM cte4 
-left JOIN cte3 ON cte3.query_name = cte4.query_name
-WHERE cte4.query_name IS NOT NULL
+select query_name, round(avg(cast(rating as decimal)/position), 2) as quality,
+ROUND(SUM(CASE WHEN rating < 3 then 1 else 0 end) * 100 / COUNT(*) ,2) as poor_query_percentage
+from Queries
+group by query_name
+having query_name is not null
