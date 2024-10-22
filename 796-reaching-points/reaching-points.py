50a82f29b841343bb4d7cf8abe318a8fb14d57d2
Yara Elkady <51079151+Yara484@users.noreply.github.com>
2024-10-23 00:12:13 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (12.01%) - LeetSync

--- 796-reaching-points/reaching-points.py
+++ 796-reaching-points/reaching-points.py
@@ -1,19 +1,10 @@
 class Solution:
-    def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
-        # 3 Cases
-        while tx >= sx and ty >= sy:
-
-            if ty == sy and tx == sx:
-                return True
-
-            if ty > tx:
-                if tx == sx:
-                    return (ty - sy) % tx == 0
-                ty %= tx
-
-            else:
-                if ty == sy:
-                    return (tx - sx) % ty == 0
-                tx %= ty   
-
-        return False             
+    def reachingPoints(self, sx, sy, tx, ty):
+        if sx > tx or sy > ty: 
+            return False 
+        if sx == tx: return (ty-sy)%sx == 0 
+        if sy == ty: return (tx-sx)%sy == 0
+        if tx > ty:
+            return self.reachingPoints(sx,sy,tx%ty,ty)
+        else:
+            return self.reachingPoints(sx,sy,tx,ty%tx)
